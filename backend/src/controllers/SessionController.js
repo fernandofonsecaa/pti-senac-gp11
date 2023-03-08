08ab565acd3e437344b3913df236ba27a5e26d25
Fernando Fonseca <ffonseca1986@gmail.com>
2023-03-08 19:54:09 -0300

    pti-004 routes configured

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,14 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async create(request, response){
+        const { id } = request.body;
+
+        const user = await connection('users').where('id', id).select('name').first();
+
+        if(!user){
+            return response.status(400).json({ error: 'No user found' });
+        }
+        return response.json(user);
+    }
+}
