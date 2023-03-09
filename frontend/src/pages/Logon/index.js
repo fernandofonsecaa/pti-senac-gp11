59bb63d8aec1d65e3489dc4731f3f2ca77fdb083
Fernando Fonseca <ffonseca1986@gmail.com>
2023-03-09 20:19:14 -0300

    validate login

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,20 +1,47 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import './styles.css';
 import { FiLogIn } from 'react-icons/fi';
+import { useState } from 'react'
+
+import api from '../../services/api'
 
 import cupImg from '../../assets/qatar.png';
 import logoImg from '../../assets/logo.png';
 
 export default function Logon(){
+    const [id, setId] = useState('')
+    const navigate = useNavigate();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        try {
+            const response = await api.post('sessions', { id });
+
+            localStorage.setItem('userId', id);
+            localStorage.setItem('userName', response.data.name);
+
+            navigate("/home");
+        
+        } catch(err) {
+            alert('Falha no login, tente novamente')
+        }
+
+    }
+
     return (
         <div className='logon-container'>
             <section className='form'>
                 <img src={logoImg} alt="logoCup"/>
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon</h1>
 
-                    <input placeholder='Sua ID' />
+                    <input 
+                        placeholder='Sua ID'
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
                     <button className='button' type='submit'>Entrar</button>
 
                     <Link className="back-link" to="/register">
